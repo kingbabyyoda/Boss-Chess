6d911b1866adeb9e0568fa7f836f330619e6d1d0
kingbabyyoda <74781719+kingbabyyoda@users.noreply.github.com>
2026-06-19 15:13:27 -0500

    Milestone 9: export trainer report and practice helpers

--- boss_chess/trainer/__init__.py
+++ boss_chess/trainer/__init__.py
@@ -1,3 +1,18 @@
 from boss_chess.trainer.analysis import Trainer
+from boss_chess.trainer.motifs import MotifResult, TacticalMotifDetector
+from boss_chess.trainer.openings import OpeningRecognizer
+from boss_chess.trainer.practice import PracticeMode
+from boss_chess.trainer.puzzles import PuzzleGenerator
+from boss_chess.trainer.report import GameReport, MoveReview, PracticePrompt
 
-__all__ = ["Trainer"]
+__all__ = [
+    "Trainer",
+    "OpeningRecognizer",
+    "TacticalMotifDetector",
+    "MotifResult",
+    "PracticeMode",
+    "PuzzleGenerator",
+    "GameReport",
+    "MoveReview",
+    "PracticePrompt",
+]
