2c3b3d132732c7f1605c41cb211fbe7debc3324c
kingbabyyoda <74781719+kingbabyyoda@users.noreply.github.com>
2026-06-19 13:34:39 -0500

    Milestone 6: export strong AI helpers

--- boss_chess/engine/__init__.py
+++ boss_chess/engine/__init__.py
@@ -1,3 +1,5 @@
 from boss_chess.engine.engine import ChessEngine
+from boss_chess.engine.opening_book import OpeningBook
+from boss_chess.engine.stockfish import StockfishAdapter
 
-__all__ = ["ChessEngine"]
+__all__ = ["ChessEngine", "OpeningBook", "StockfishAdapter"]
