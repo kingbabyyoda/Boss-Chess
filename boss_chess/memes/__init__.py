6a9867c8b06fea9301c98a385bc75af7d8e4ee78
kingbabyyoda <74781719+kingbabyyoda@users.noreply.github.com>
2026-06-19 14:55:46 -0500

    Milestone 8: export meme subsystem helpers

--- boss_chess/memes/__init__.py
+++ boss_chess/memes/__init__.py
@@ -1,3 +1,13 @@
+from boss_chess.memes.cache import MemeCache
+from boss_chess.memes.personalities import PERSONALITIES, PERSONALITY_INTROS, VOICE_TAGS
 from boss_chess.memes.provider import MemeProvider
+from boss_chess.memes.reddit import RedditTitleFetcher
 
-__all__ = ["MemeProvider"]
+__all__ = [
+    "MemeCache",
+    "MemeProvider",
+    "RedditTitleFetcher",
+    "PERSONALITIES",
+    "PERSONALITY_INTROS",
+    "VOICE_TAGS",
+]
