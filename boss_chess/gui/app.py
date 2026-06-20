a93e108a35b9f6f6ae022806d6959f568b2744da
kingbabyyoda <74781719+kingbabyyoda@users.noreply.github.com>
2026-06-20 09:58:05 -0500

    Milestone 12: launch the production shell GUI

--- boss_chess/gui/app.py
+++ boss_chess/gui/app.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 
+from boss_chess.gui.production_shell import ProductionBossChessApp
 from boss_chess.gui.session import GuiSession
 from boss_chess.gui.splash import show_splash
-from boss_chess.gui.window import BossChessApp
 from boss_chess.types import GameConfig
 
 
 def run_gui(config: GameConfig) -> None:
     show_splash()
-    BossChessApp(GuiSession(config)).run()
+    ProductionBossChessApp(GuiSession(config)).run()
